87246d1e8f8f3d9881b7dab4b2637cac120eb5cc
omerzk <mine!>
2016-05-29 17:01:34 +0300

    intial front

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require("express-session");
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -20,10 +20,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/public',express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

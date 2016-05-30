bbfb8b13715df4104033dde3523eaf3a7acee393
omerzk <mine!>
2016-05-30 12:59:57 +0300

    Merge branch 'master' of https://github.com/omerzk/HoldTheDoor
    Conflicts:
    	app.js

--- app.js
+++ app.js
@@ -2,12 +2,55 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var connect = require('connect');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var session = require('express-session');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://thrisno:clod@ds036789.mlab.com:36789/dibi');
+var db = mongoose.connection;
+var dbReady = false;
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("DB ready");
+  dbReady = true;
+});
+
+// Configure the local strategy for use by Passport.
+passport.use(new Strategy(
+    function (username, password, cb) {
+      db.users.findByUsername(username, function (err, user) {
+        if (err) {
+          return cb(err);
+        }
+        if (!user) {
+          return cb(null, false);
+        }
+        if (user.password != password) {
+          return cb(null, false);
+        }
+        return cb(null, user);
+      });
+    }));
+
+
+// Configure Passport authenticated session persistence.
+passport.serializeUser(function (user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+
 
 var app = express();
 
@@ -15,104 +58,71 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true },
-  secret: 'ssshhhhh'
+  cookie: {secure: true},
+  secret: 'ko ko ka cho'
 }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/public',express.static(path.join(__dirname, 'public')));
-app.use('/', routes);
-app.use('/users', users);
 
-
-app.set('views', __dirname + '/views');
-app.engine('html', require('ejs').renderFile);
 
 var sess;
-var userArray;
+var userArray = {};
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', function (req, res) {
+  res.render('enter.jade');
 });
 
-// error handlers
+//to remove auth make failureRedirect direct to wherever you want
+app.post('/login', passport.authenticate('local', {failureRedirect: '/', successRedirect: '/games'}));
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+//also comment-out this line
+app.all('*', require('connect-ensure-login').ensureLoggedIn('/'), function (a, b, next) {
+  next();
 });
 
-app.get('/',function(req,res){
+app.get('/games', function (req, res) {
   sess = req.session;
-  req.session.id
-  // If name exists redirect to games
-  // else render index.html
-  if(userArray[sess.id]) {
-    /*
-     * This line check Session existence.
-     * If it existed will do some action.
-     */
-    res.redirect('/games');
-  } else {
-    res.render('enter.jade');
-  }
-});
+  console.log(sess.id);
+  console.log(userArray);
 
-app.post('/login',function(req,res){
-  sess = req.session;
-  userArray[sess.id] = req.body.name;
-  res.end('done');
-});
-
-app.get('/games',function(req,res){
-  sess = req.session;
-  if(userArray[sess.id]) {
+  if (sess.id in userArray) {
     res.render('games.jade');
   } else {
     res.render('enter.jade');
   }
 });
 
-app.get('/games/active',function(req,res){
+app.get('/games/active', function (req, res) {
   res.json(userArray)
 });
 
-app.get('/games/new',function(req,res){
+app.get('/games/new', function (req, res) {
   res.render('new_game.jade');
 });
 
-app.get('/game',function(req,res){
+app.get('/game', function (req, res) {
   res.render('game.jade');
 });
 
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 module.exports = app;
